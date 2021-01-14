4e53905cb4e04f301823ea7422658929e52e2223
DKVASDES@carlsberggroup.com <DKVASDES@carlsberggroup.com>
2021-01-14 14:16:05 +0100

    LoginQuiz: Remove warnings

--- QuizUtil.php
+++ QuizUtil.php
@@ -1,40 +1,43 @@
 <?php
-    
+
 class QuizUtil {
-    
+
     public function getInputType($type) {
-        
-        if ($type == "RADIO_BUTTON") { 
+
+        if ($type == "RADIO_BUTTON") {
             echo 'radio';
-        } else { 
+        } else {
             echo 'checkbox';
         }
     }
-    
+
      public function getInputTextType($type) {
-        
-        if ($type == "TEXTAREA") { 
+
+        if ($type == "TEXTAREA") {
             echo 'textarea';
-        } else { 
+        } else {
             echo 'text';
         }
     }
-    
+
     public function getInsertQueryForResponses($question, $response, $respondentId) {
-        
+
         $qArray = explode('_', $question);
-                
+
         if (count($qArray) == 1) {
             return "('$respondentId', '$qArray[0]', null, '$response', null), ";
         }
-        
+
         if (count($qArray) == 2) {
             return "('$respondentId', '$qArray[0]', '$response', null, null), ";
         }
-        
+
         if (count($qArray) == 3) {
             return "('$respondentId', '$qArray[0]', null, '$response', '$qArray[1]'), ";
         }
+
+        return "";
     }
 }
+
 ?>
